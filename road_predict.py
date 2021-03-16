5168ef87ee8c6babf510383c52e6f130d8e6a08d
shao_218 <1740440084@qq.com>
2021-03-16 17:11:30 +0800

    update data before testing

--- road_predict.py
+++ road_predict.py
@@ -5,16 +5,26 @@ from road_frcnn import FRCNN
 from PIL import Image
 
 frcnn = FRCNN()
+img = r'train_road_faster_rcnn/0_7790_5852.jpg'
+# img = input('Input image filename:')
+image = Image.open(img)
+print('Open Error! Try again!')
+r_image = frcnn.detect_image(image)
+r_image.show()
 
-while True:
-    img = r'train_road_faster_rcnn/0_3899_4084.jpg'
-    # img = input('Input image filename:')
 
-    try:
-        image = Image.open(img)
-    except:
-        print('Open Error! Try again!')
-        continue
-    else:
-        r_image = frcnn.detect_image(image)
-        r_image.show()
+# while True:
+#     try:
+#         image = Image.open(img)
+#     except:
+#         print('Open Error! Try again!')
+#         continue
+#     else:
+#         r_image = frcnn.detect_image(image)
+#         r_image.show()
+img = r'train_road_faster_rcnn/0_7790_5852.jpg'
+# img = input('Input image filename:')
+image = Image.open(img)
+print('Open Error! Try again!')
+r_image = frcnn.detect_image(image)
+r_image.show()
